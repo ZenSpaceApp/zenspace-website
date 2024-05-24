44d491f8ff56b8773a4781fc2a38624040c35110
rup2701 <rupesh@creativegray.com>
2024-05-24 10:37:30 -0700

    Added back to home and sign up options on 404 page.

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import YinYangIcon from '../public/images/icons/yin-yang.svg';
+import Link from "next/link";
 
 export default function Show404() {
   return <div className="show fade"
@@ -24,7 +25,23 @@ export default function Show404() {
     />
       <h2>We&apos;re sorry, this page isn&apos;t ready yet.</h2>
       <p>Our team is working behind the scenes to bring ZenSpaceApp
-        to life. Sign up for early access to be notified when we launch!</p>
+        to life. <Link
+        href="./"
+        style={{
+          color: "wheat !important",
+          fontWeight: "700",
+          borderBottom: "2px solid"
+        }}>Sign up</Link> for early access to be notified when we launch!</p>
+      
+      <Link
+        href="./"
+        style={{
+          paddingTop: "3rem !important",
+          color: "white !important",
+          fontWeight: "500",
+        }}>
+        Back home ...
+      </Link>
     </div>
   </div>
 }
