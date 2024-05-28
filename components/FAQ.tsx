71b9d03d600bc6bd09f6bd01502baa2d4236df5e
rup2701 <rupesh@creativegray.com>
2024-05-28 08:40:05 -0700

    Changed platform to app in the FAQs.

--- components/FAQ.tsx
+++ components/FAQ.tsx
@@ -5,7 +5,7 @@ import { block } from "sharp";
 const faqs = [
   [
     "What is ZenSpaceApp and how does it work?",
-    "ZenSpaceApp is an innovative platform designed to help mental health professionals\
+    "ZenSpaceApp is an innovative app designed to help mental health professionals\
     streamline their practice management, enhance patient communication, and simplify\
     therapy documentation. By combining telehealth capabilities, AI-powered therapy notes,\
     scheduling features, and integrated patient communication tools into a single,\
@@ -14,7 +14,7 @@ const faqs = [
   ],
   [
     "How secure is my data on ZenSpaceApp?",
-    "At ZenSpaceApp, we take data security and privacy seriously. Our platform is fully HIPAA-compliant,\
+    "At ZenSpaceApp, we take data security and privacy seriously. Our app is fully HIPAA-compliant,\
     ensuring that we meet the highest standards for safeguarding sensitive patient information.\
     We implement robust security measures, including end-to-end encryption for data in transit and at rest. This means that your data is encrypted when it's being transferred between your device and our servers, as well as when it's stored on our servers.\
     In addition to encryption, we regularly monitor our systems for potential threats and vulnerabilities to maintain a secure environment for your practice's data. You can trust that your information is protected and secure when using ZenSpaceApp."
@@ -23,8 +23,8 @@ const faqs = [
     "How easy is it to get started with ZenSpaceApp?",
     "ZenSpaceApp is designed to be user-friendly and easy to get started with. The sign-up process\
      is quick, taking just a few minutes, and the onboarding process will guide you through the\
-      platform's features and help you customize your settings to best suit your practice's needs.\
-       The platform's clean and organized design makes it simple to navigate and access all of its features, while our dedicated support team is always available to assist you. With these features, you can quickly and easily start using ZenSpaceApp to streamline your practice management, enhance patient communication, and simplify therapy documentation."
+      app's features and help you customize your settings to best suit your practice's needs.\
+       The app's clean and organized design makes it simple to navigate and access all of its features, while our dedicated support team is always available to assist you. With these features, you can quickly and easily start using ZenSpaceApp to streamline your practice management, enhance patient communication, and simplify therapy documentation."
   ],
   [
     "Does ZenSpaceApp integrate with my existing therapy tools and workflows?",
@@ -34,7 +34,7 @@ const faqs = [
     you to manage your appointments and client bookings within ZenSpaceApp while keeping your\
     existing calendars up-to-date.\
     Additionally, ZenSpaceApp integrates with common payment gateways such as PayPal and Stripe,\
-    enabling you to manage client payments directly within the platform. By offering these integrations,\
+    enabling you to manage client payments directly within the app. By offering these integrations,\
     we aim to streamline your practice management and simplify your daily tasks, allowing you t\
      focus on providing exceptional care to your clients."
   ],
@@ -51,10 +51,10 @@ const faqs = [
     "Can I try ZenSpaceApp before committing to a subscription?",
     "Absolutely! We believe in letting potential users experience the full potential of ZenSpaceApp\
     before committing to a subscription. That's why we offer a 30-day free trial, allowing you to\
-    test out all of the platform's features and ensure it's the right fit for your practice.\
+    test out all of the app's features and ensure it's the right fit for your practice.\
     During the trial period, you'll have access to all of ZenSpaceApp's functionality, including\
     telehealth, AI-powered therapy notes, scheduling, and patient communication tools. This gives\
-    you plenty of time to explore the platform and see how it can benefit your practice.\
+    you plenty of time to explore the app and see how it can benefit your practice.\
     In some cases, such as for Lighthouse customers, we may be able to extend the trial period\
      beyond 30 days. If you're interested in a longer trial, feel free to reach out to our support\
       team to discuss your options."
